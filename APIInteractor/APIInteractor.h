c2e171f5cb97342aa8bf7471ba1855b8d78ea9fe
Ethan Borrowman <ethan_borrowman@hotmail.com>
2024-07-08 16:52:56 -0500

    Initial Commit

---
+++ APIInteractor/APIInteractor.h
@@ -0,0 +1,18 @@
+//
+//  APIInteractor.h
+//  APIInteractor
+//
+//  Created by Ethan Borrowman on 7/8/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIInteractor.
+FOUNDATION_EXPORT double APIInteractorVersionNumber;
+
+//! Project version string for APIInteractor.
+FOUNDATION_EXPORT const unsigned char APIInteractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIInteractor/PublicHeader.h>
+
+
---
+++ APIInteractorTests/APIInteractorTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIInteractorTests.swift
+//  APIInteractorTests
+//
+//  Created by Ethan Borrowman on 7/8/24.
+//
+
+import Testing
+@testable import APIInteractor
+
+struct APIInteractorTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
